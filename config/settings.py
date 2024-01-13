11efac4f578fe8554fdde72470208a0974ae42aa
AbdusattarovOtabek <oabdusattarov888@gmail.com>
2024-01-13 15:57:09 +0500

    debug change

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(pf$acx79yycx_gr#_i#h*xez1f&ch#20k@hxo(ixxt@$3#vi9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
